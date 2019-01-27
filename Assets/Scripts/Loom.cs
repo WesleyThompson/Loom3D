1b4316abc6c035ae6858cfa16f09795bcad90866
Wesley Thompson <wesleythompson1995@gmail.com>
2019-01-27 00:59:19 -0500

    Added vertice generation

--- Assets/Scripts/Loom.cs
+++ Assets/Scripts/Loom.cs
@@ -11,7 +11,7 @@ public class Loom : MonoBehaviour {
     public float segmentSize;
 
     private float[,] imageSegments;
-    private Vector3[] vertices;
+    private List<Vector3> vertices;
 
     private void Awake()
     {
@@ -20,7 +20,23 @@ public class Loom : MonoBehaviour {
 
         Debug.Log(CalculateSegmentDimensions(segmentResolution, texture));
         CalculateImageSegments(ref imageSegments, segmentResolution, texture);
+        CalculateVertices(ref vertices, imageSegments, segmentSize);
+    }
 
+    private void OnDrawGizmos()
+    {
+        if(vertices == null)
+        {
+            return;
+        }
+        else
+        {
+            Gizmos.color = Color.black;
+            for (int i = 0; i < vertices.Count; i++)
+            {
+                Gizmos.DrawSphere(vertices[i], 0.05f);
+            }
+        }
     }
 
     /// <summary>
@@ -61,5 +77,66 @@ public class Loom : MonoBehaviour {
             }
         }
 
+    }
+
+    private void CalculateVertices(ref List<Vector3> vertices, float[,] imageSegments, float segmentSize)
+    {
+        vertices = new List<Vector3>();
+
+        for(int i = 0; i < imageSegments.GetLength(0); i++)
+        {
+            for(int j = 0; j < imageSegments.GetLength(1); j++)
+            {
+                if(imageSegments[i,j] > 0)
+                {
+                    //Create 4 vectors for our square segment
+                    float rCoord = (i + 1) * segmentSize;
+                    float cCoord = (j + 1) * segmentSize;
+                    Vector3 bottomLeft = new Vector3(i * segmentSize, j * segmentSize);
+                    Vector3 bottomRight = new Vector3(rCoord, j * segmentSize);
+                    Vector3 topLeft = new Vector3(i * segmentSize, cCoord);
+                    Vector3 topRight = new Vector3(rCoord, cCoord); //This one will always be a new coordinate
+
+                    //Make sure none of our vectors are already in our vector list
+                    bool bL = false;
+                    bool bR = false;
+                    bool tL = false;
+                    foreach(Vector3 v in vertices)
+                    {
+                        if(!bL && v == bottomLeft)
+                        {
+                            bL = true;
+                        }
+                        else if(!bR && v == bottomRight)
+                        {
+                            bR = true;
+                        }
+                        else if(!tL && v == topLeft)
+                        {
+                            tL = true;
+                        }
+
+                        if(bL && bR && tL)
+                        {
+                            break;
+                        }
+                    }
+                    //If not add it
+                    vertices.Add(topRight);
+                    if(!bL)
+                    {
+                        vertices.Add(bottomLeft);
+                    }
+                    if(!bR)
+                    {
+                        vertices.Add(bottomRight);
+                    }
+                    if(!tL)
+                    {
+                        vertices.Add(topLeft);
+                    }
+                }
+            }
+        }
     }
 }
